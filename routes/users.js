b1557ee26dd95b5b563a965ea3c91d994c596d7a
AJ Dalbany Garrett <ajdg8811@gmail.com>
2023-06-09 09:02:15 -0400

    first draft users stuff

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+const users = {
+  1: {
+    id: 001,
+    username: 'Bartholomew D. Lester',
+  },
+  2: {
+    id: 002,
+    username: 'Frances Grayson'
+    },
+};
+
+const posts = {
+  1: {
+    id: 001,
+    date:"" ,
+    text: "" ,
+    userID:"",
+  },
+  2: {
+    id: 002,
+    date:"" ,
+    text: "" ,
+    userID:"",
+  },
+};
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
 });
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+  
+})
+
+app.listen(process.env.PORT,() => 
+  res.send(`your post has been uploaded ${process.env.PORT}!`),
+);
 
 module.exports = router;
